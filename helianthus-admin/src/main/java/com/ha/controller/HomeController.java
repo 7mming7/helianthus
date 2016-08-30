9e251d3ceb645356fe62e632913238f9ac517156
shuiqing <shuiqing301@gmail.com>
2016-08-30 18:38:08 +0800

    页面添加页面规则处理的feature.

--- helianthus-admin/src/main/java/com/ha/controller/HomeController.java
+++ helianthus-admin/src/main/java/com/ha/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.ha.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -37,4 +38,5 @@ public class HomeController {
         response.addCookie(cookie);
         return new ModelAndView("test");
     }
+
 }
---
+++ helianthus-admin/src/main/java/com/ha/controller/PageRuleController.java
@@ -0,0 +1,31 @@
+package com.ha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 页面规则配置
+ * User: shuiqing
+ * DateTime: 16/8/29 下午10:45
+ * Email: helianthus301@163.com
+ * GitHub: https://github.com/shuiqing301
+ * Blog: http://shuiqing301.github.io/
+ * _
+ * |_)._ _
+ * | o| (_
+ */
+@Controller
+public class PageRuleController {
+
+    @RequestMapping("/pageRuleManagement")
+    public ModelAndView pageRuleManagement() {
+        return new ModelAndView("rule_management");
+    }
+
+    @RequestMapping(value = "/addPageInfo", method = RequestMethod.GET)
+    public ModelAndView addPageInfo() {
+        return new ModelAndView("add_page_info");
+    }
+}
