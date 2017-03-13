1be4a878cd5c9d0171989a8e904ce62b16b2ce2f
shuiqing <shuiqing301@gmail.com>
2017-03-13 23:37:41 +0800

    Some features.

---
+++ helianthus-core/src/main/java/com/ha/base/Constants.java
@@ -0,0 +1,37 @@
+package com.ha.base;
+
+/**
+ * 常量
+ * User: shuiqing
+ * DateTime: 17/3/5 下午1:21
+ * Email: annuus.sq@gmail.com
+ * GitHub: https://github.com/shuiqing301
+ * Blog: http://shuiqing301.github.io/
+ * _
+ * |_)._ _
+ * | o| (_
+ */
+public interface Constants {
+
+    // ================================
+
+    // 任务正在运行
+    public static final int JOB_RUNNING = 1;
+
+    // 任务完成
+    public static final int JOB_SUCCESS = 2;
+
+    // 任务失败
+    public static final int JOB_ERROR = 3;
+
+    // 任务被kill掉
+    public static final int JOB_KILL = 4;
+
+    // 任务延期
+    public static final int JOB_DELAY = 5;
+
+    // ================================
+
+
+
+}
---
+++ helianthus-core/src/main/java/com/ha/parser/BasicParser.java
@@ -0,0 +1,19 @@
+package com.ha.parser;
+
+import com.ha.config.SysConfigAble;
+import org.apache.hadoop.util.Tool;
+
+/**
+ * 解析器接口
+ * User: shuiqing
+ * DateTime: 17/3/5 下午1:18
+ * Email: annuus.sq@gmail.com
+ * GitHub: https://github.com/shuiqing301
+ * Blog: http://shuiqing301.github.io/
+ * _
+ * |_)._ _
+ * | o| (_
+ */
+public interface BasicParser extends Tool,SysConfigAble {
+
+}
