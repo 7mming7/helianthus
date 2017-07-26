e7ab19bda51474e95899f96d213b7b09cf7b9145
shuiqing <shuiqing301@gmail.com>
2017-07-26 16:24:48 +0800

    添加helianthus-import project flow node页面管理功能。

---
+++ helianthus-pnc2admin/src/main/java/com/ha/system/service/IFileStoreService.java
@@ -0,0 +1,20 @@
+package com.ha.system.service;
+
+import com.ha.system.domain.FileStore;
+
+/**
+ * User: shuiqing
+ * DateTime: 17/4/25 下午2:17
+ * Email: annuus.sq@gmail.com
+ * GitHub: https://github.com/shuiqing301
+ * Blog: http://shuiqing301.github.io/
+ * _
+ * |_)._ _
+ * | o| (_
+ */
+public interface IFileStoreService {
+
+    boolean addOrUpdateFileStore(FileStore fileStore);
+
+    Iterable<FileStore> findAll();
+}
---
+++ helianthus-pnc2job/src/main/java/com/ha/graph/node/NodeService.java
@@ -0,0 +1,29 @@
+package com.ha.graph.node;
+
+import com.ha.base.BaseService;
+import com.ha.inject.annotation.BaseComponent;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * User: shuiqing
+ * DateTime: 17/6/7 下午2:27
+ * Email: annuus.sq@gmail.com
+ * GitHub: https://github.com/shuiqing301
+ * Blog: http://shuiqing301.github.io/
+ * _
+ * |_)._ _
+ * | o| (_
+ */
+@Service
+@Getter
+@Setter
+public class NodeService extends BaseService<Node, Long> {
+
+    @BaseComponent
+    @Autowired
+    private NodeRepository nodeRepository;
+
+}
